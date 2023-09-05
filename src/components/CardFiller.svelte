ac387ca2df14e56bf6333f2cf62800caec36f74c
ViperBite03 <layitamartin@gmail.com>
2023-09-05 03:19:02 +0200

    button

--- src/components/CardFiller.svelte
+++ src/components/CardFiller.svelte
@@ -1,56 +1,99 @@
 <script lang="ts">
-    let month
-    let year
+  let month
+  let year
 
-    /*for(var m = 0; m < 12; m++){
+  /*for(var m = 0; m < 12; m++){
         console.log(m)
     }
     for(var i = 23; i < 30; i++){
         console.log(i)
     }*/
-
 </script>
 
 <style lang="scss">
-    .card-filler{
-        position: relative;
-        display: flex;
-        justify-content: center;
-        height: 754px;
+  * {
+    //border: 1px solid red;
+  }
 
-        .card{
-            position: absolute;
-            z-index: 1;
-            margin-top: 50px;
-            
-            width: 323px;
-            height: 204px;
-            border-radius: 13px;
-            background-color: #89CCC5;
-            box-shadow: 0px 10px 45px -27px rgba(0,0,0,1);
-        }
-        
-        .info{
-            position: relative;
-            top: 152px;
+  .card-filler {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    height: 730px;
 
-            height: 500px;
-            width: 500px;
-            border-radius: 10px;
+    .card {
+      position: absolute;
+      z-index: 1;
+      margin-top: 50px;
 
-            background-color: #D7EBE9;
-            //box-shadow: 0px -14px 40px -20px rgba(0,0,0,0.75);
-        }
+      width: 323px;
+      height: 204px;
+      border-radius: 13px;
+      background-color: #89ccc5;
+      box-shadow: 0px 10px 45px -27px rgba(0, 0, 0, 1);
     }
+
+    .info {
+      position: relative;
+      justify-content: center;
+      display: flex;
+      align-items: center;
+      top: 152px;
+
+      height: 500px;
+      width: 500px;
+      border-radius: 10px;
+
+      background-color: #d7ebe9;
+
+      .info-inputs {
+        height: fit-content;
+        display: grid;
+        margin-top: 100px;
+
+        justify-content: center;
+        grid-template-columns: 1fr 1fr;
+        padding: 20px;
+
+        input {
+          top: 50%;
+          height: 40px;
+          border-radius: 8px;
+          border: 1px;
+          margin: 12px;
+          padding: 20px;
+        }
+
+        .card-number,
+        .name {
+          grid-column: 1/3;
+        }
+
+        button {
+          grid-column: 1/3;
+          margin: 12px;
+          margin-top: 20px;
+          height: 35px;
+          cursor: pointer;
+          background-color: #e2958c;
+
+          border-radius: 8px;
+          border: 0;
+        }
+      }
+    }
+  }
 </style>
 
-<dev class="card-filler">
-    <dev class="card"></dev>
-    <dev class="info">
-        <!--CENTRAR ESTA PUTA MIERDA-->
-        <input type="password" class="card-number"> 
-        <input type="text" class="name">
-        <input type="text" class="cvv">
-        <input type="month"/>
-    </dev>
-</dev>
+<div class="card-filler">
+  <div class="card" />
+  <div class="info">
+    <div class="info-inputs">
+      <input type="password" class="card-number" />
+      <input type="text" class="name" />
+      <input type="month" />
+      <input type="text" class="cvv" />
+      <button>Submit</button>
+    </div>
+  </div>
+</div>

578ae4e522b56f497dd18c6defdf0d6b124e87d7
ViperBite03 <layitamartin@gmail.com>
2023-11-15 00:57:31 +0100

    defghjk

--- src/components/Galeria.svelte
+++ src/components/Galeria.svelte
@@ -10,7 +10,7 @@
     justify-content: center;
     align-items: center;
     position: fixed;
-    background-color: rgb(32, 32, 32);
+    background-color: rgb(19, 38, 47);
     width: 100%;
     height: 100vh;
 
@@ -22,14 +22,13 @@
       margin: 0 auto;
       top: 50px;
       color: white;
-      font-size: 15px;
+      font-size: 20px;
     }
 
     .image-container {
       overflow: hidden;
       width: 50%;
       max-width: 1000px;
-      border-radius: 20px;
 
       .images {
         width: 100%;
@@ -51,12 +50,36 @@
 
     button {
       position: absolute;
-      width: 50px;
-      height: 50px;
+      width: 60px;
+      height: 60px;
       border-radius: 50%;
-      border: none;
-      background-color: white;
       top: calc(50% - 25px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 3px solid white;
+      overflow: hidden;
+      background-color: inherit;
+      z-index: 99;
+      cursor: pointer;
+
+      .circle {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        position: absolute;
+        background-color: white;
+        transform: translateX(50px) translateY(50px);
+        border: 2px solid white;
+        transition: 0.2s ease;
+      }
+
+      &:hover {
+        .circle {
+          transition: 0.3s ease;
+          transform: translateX(0px) translateY(0px);
+        }
+      }
     }
   }
 </style>
@@ -72,6 +95,18 @@
     </div>
   </div>
   <div class="contador">{i + 1} / {photos.length}</div>
-  <button class="previous" style="left: 75px;" on:click={() => (i = i === 0 ? photos.length - 1 : i - 1)} />
-  <button class="next" style="right: 75px;" on:click={() => (i = i === photos.length - 1 ? 0 : i + 1)} />
+  <button class="previous" style="left: 75px;" on:click={() => (i = i === 0 ? photos.length - 1 : i - 1)}>
+    <div class="arrow" style="z-index: 99;">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+        <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+      </svg>
+    </div>
+    <div class="circle" />
+  </button>
+  <button class="next" style="right: 75px;" on:click={() => (i = i === photos.length - 1 ? 0 : i + 1)}>
+    <svg class="arrow" style="z-index: 99;" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+      <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+    </svg>
+    <div class="circle" />
+  </button>
 </div>

b5eb66d31a6cdfdaa56eb25547c2ee95b35bd8eb
ViperBite03 <layitamartin@gmail.com>
2024-01-19 13:59:43 +0100

    opinions mejorado

--- src/components/Opinions.svelte
+++ src/components/Opinions.svelte
@@ -15,6 +15,11 @@
       text: 'Empresa muy profesional. Ponen todos los medios para dar a conocer el inmueble de acuerdo con los propietarios. Seguimiento exhaustivo. Respuesta inmediata del gerente ante cualquier consulta lo que da tranquilidad y seguridad.',
       stars: 2,
     },
+    {
+      name: 'Miquel Toran',
+      text: 'Empresa muy profesional.',
+      stars: 4,
+    },
   ]
 
   let currentOpinion: number = 0
@@ -29,8 +34,6 @@
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.cdnfonts.com/css/kabos-gyula');
-
   .container {
     height: 50vh;
     width: 100%;
@@ -77,6 +80,7 @@
           position: relative;
           align-self: end;
 
+          z-index: 2;
           margin-right: 35px;
 
           .rating {
@@ -111,22 +115,19 @@
         .L-quote,
         .R-quote {
           height: 50%;
-
-          font-family: 'Kabos Gyula', sans-serif;
           opacity: 0.1;
-          font-size: 250px;
           position: absolute;
         }
 
         .L-quote {
           top: -20px;
-          left: -70px;
-          transform: scale(-1);
+          left: -40px;
         }
 
         .R-quote {
+          transform: scale(-1);
           bottom: -20px;
-          right: -70px;
+          right: -40px;
         }
 
         .text {
@@ -185,7 +186,14 @@
   <div class="opinions">
     {#each opinions as opinion, index}
       <div class="opinion" class:active={currentOpinion === index}>
-        <div class="L-quote">"</div>
+        <div class="L-quote">
+          <svg xmlns="http://www.w3.org/2000/svg" width="151.28" height="128" viewBox="0 0 1664 1408"
+            ><path
+              fill="#000000"
+              d="M768 832v384q0 80-56 136t-136 56H192q-80 0-136-56T0 1216V512q0-104 40.5-198.5T150 150T313.5 40.5T512 0h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136m896 0v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136V512q0-104 40.5-198.5T1046 150t163.5-109.5T1408 0h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136"
+            /></svg
+          >
+        </div>
         <div class="name">{opinion.name}</div>
         <div class="text">{opinion.text}</div>
         <div class="stars">
@@ -213,7 +221,14 @@
             </div>
           {/each}
         </div>
-        <div class="R-quote">"</div>
+        <div class="R-quote">
+          <svg xmlns="http://www.w3.org/2000/svg" width="151.28" height="128" viewBox="0 0 1664 1408"
+            ><path
+              fill="#000000"
+              d="M768 832v384q0 80-56 136t-136 56H192q-80 0-136-56T0 1216V512q0-104 40.5-198.5T150 150T313.5 40.5T512 0h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136m896 0v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136V512q0-104 40.5-198.5T1046 150t163.5-109.5T1408 0h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136"
+            /></svg
+          >
+        </div>
       </div>
     {/each}
   </div>
